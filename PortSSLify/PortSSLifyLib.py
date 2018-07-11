8b2e23ff652d366ae2a07c253f1365c34578600d
FriesW <william.james.fries@gmail.com>
2018-07-10 21:07:29 -0500

    Add debugging

--- PortSSLify/PortSSLifyLib.py
+++ PortSSLify/PortSSLifyLib.py
@@ -2,7 +2,7 @@ import socket, ssl, threading, time
 
 _debug_level = -1
 def _pd(dl, *args):
-    if _debug_level < dl:
+    if _debug_level > dl or _debug_level < 0:
         print( time.strftime('%x %X %Z :: ') + ' '.join([str(e) for e in args]) )
 
 class PortSSLify:
@@ -26,21 +26,26 @@ class PortSSLify:
         self.context.load_cert_chain(certfile=certfile_path, keyfile=keyfile_path)
     
     def start(self):
+        _pd(0, 'Starting up server...')
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
             sock.bind(self.bind_addr)
             sock.listen(self.queue_size)        
             with self.context.wrap_socket(sock, server_side=True) as sslsock:
+                _pd(0, 'Server successfully started.')
                 while True:
                     self.active.acquire()
                     try:
+                        addr = "'connection failure'"
                         ibc, addr = sslsock.accept()
-                        _pd(1, 'Connection from', addr)
+                        _pd(2, 'Connection from', addr)
                         obc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
                         obc.connect(self.forward_addr)
+                        _pd(3, 'Success connecting to', self.forward_addr)
                         conns = _connections(ibc, obc, self.timeout, self.active.release)
                         _send(conns).start()
                         _recv(conns).start()
-                    except:
+                    except Exception as e:
+                        _pd(1, 'Building bridge for client', addr, 'encountered error:', repr(e))
                         try: ibc.shutdown(socket.SHUT_RDWR)
                         finally: pass
                         try: obc.shutdown(socket.SHUT_RDWR)
@@ -67,6 +72,7 @@ class _connections:
         close_send.shutdown(socket.SHUT_WR)
         self.__s += 1
         if self.__s == 2:
+            _pd(2, 'Closing connection for client', self.ibc.getpeername())
             self.ibc.close()
             self.obc.close()
             if self.ext_method != None:
@@ -88,6 +94,7 @@ class _transfer(threading.Thread):
                 s.sendall(data)
                 data = recv()
         finally:
+            _pd(3, 'transfer thread', "'"+type(self).__name__+"'", 'is exiting for client', self.state.ibc.getpeername())
             self.state.exit(r, s)
 
 
