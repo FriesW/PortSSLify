10414292c041cf5c424cf865566263fecc63e9a1
FriesW <william.james.fries@gmail.com>
2018-07-10 20:31:40 -0500

    Possibly complete socket relay

--- PortSSLify/PortSSLifyLib.py
+++ PortSSLify/PortSSLifyLib.py
@@ -10,15 +10,16 @@ class PortSSLify:
     def __init__(self,
                  certfile_path = 'cert.pem', keyfile_path = 'key.pem',
                  bind = ('127.0.0.1', 443), forward = ('127.0.0.1', 80),
-                 timeout = 60, max_active = 10, socket_queue = 2,
+                 max_active = 10, socket_queue = 2,
                  ssl_protocol = ssl.PROTOCOL_TLSv1_2,
+                 timeout = 10,
                  debug_level = -1):
         self.bind_addr = bind
         self.forward_addr = forward
         self.active = threading.Semaphore(max_active)
-        self.timeout = timeout
         self.queue_size = socket_queue
         self.protocol = ssl_protocol
+        self.timeout = timeout
         _debug_level = debug_level
         
         self.context = ssl.SSLContext(self.protocol)
@@ -36,7 +37,7 @@ class PortSSLify:
                         _pd(1, 'Connection from', addr)
                         obc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
                         obc.connect(self.forward_addr)
-                        conns = __connections(ibc, obc)
+                        conns = __connections(ibc, obc, self.timeout, self.active.release)
                         __send(conns).start()
                         __recv(conns).start()
                     except:
@@ -44,24 +45,26 @@ class PortSSLify:
 
     
     class __connections:
-        def __init__(self, inbound_conn, outbound_conn):
+        def __init__(self, inbound_conn, outbound_conn, timeout = 60, call_on_completion = None):
             self.ibc = inbound_conn
             self.obc = outbound_conn
-            self.__s = 2
+            self.ibc.settimeout(timeout)
+            self.obc.settimeout(timeout)
+            self.ext_method = call_on_completion
+            self.__s = 0
         
         def status(self):
-            return bool(self.__s)
+            return self.__s == 0
         
         def exit(self, close_recv, close_send):
-            self.__s -= 1
-            try: close_recv.shutdown(socket.SHUT_RD)
-            finally: pass
-            try: close_send.shutdown(socket.SHUT_WR)
-            finally: pass
-            if not self.__s:
+            close_recv.shutdown(socket.SHUT_RD)
+            close_send.shutdown(socket.SHUT_WR)
+            self.__s += 1
+            if self.__s == 2:
                 self.ibc.close()
                 self.obc.close()
-                self.active.release()
+                if self.ext_method != None:
+                    self.ext_method()
         
     
     class __transfer(threading.Thread):
@@ -69,13 +72,17 @@ class PortSSLify:
             self.state = state
         
         def run_as(self, r, s):
+            def recv():
+                while self.state.status():
+                    try: return r.recv(1024)
+                    except socket.timeout: pass
             try:
-                data = r.recv(1024)
-                while data and self.state.status():
+                data = recv()
+                while data:
                     s.sendall(data)
-                    data = r.recv(1024)
+                    data = recv()
             finally:
-                self.state.exit(r, s) # <- no good. Need to swap r and s depending upon where error came from
+                self.state.exit(r, s)
                 
     
     class __send(__transfer):
